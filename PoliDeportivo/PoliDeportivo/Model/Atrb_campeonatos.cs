8f4730d529b03fad41b4eb70aa1f0fbb2ce551f6
ErnestoSamayoa2401 <esamayoaj1@miumg.edu.gt>
2025-08-01 15:33:11 -0600

    Agregado de formulario de campeonatos

---
+++ PoliDeportivo/PoliDeportivo/Model/Atrb_campeonatos.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoliDeportivo.Model
+{
+    public class Atrb_campeonatos
+    {
+        public int pk_campeonato_id { get; set; }
+        public string cam_nombre { get; set; }
+        public string cam_modalidad { get; set; }
+        public int cam_cant_equipos { get; set; }
+        public DateTime cam_fecha_inicio { get; set; }
+        public DateTime cam_fecha_final { get; set; }
+        public int jor_cant_partidos { get; set; }
+    }
+}
